22798596d3faf36cd584a360d470687c1a83c450
devRavindar <yogiravi0987@gmail.com>
2023-04-03 20:47:25 +0530

    just a initail commit

--- src/components/Landingpage.jsx
+++ src/components/Landingpage.jsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom"
+
 const Landingpage=()=>{
     return(
 <div className="landingpage">
-    <h1>hwwwddsdijfhusdhfiushfuhbsoj</h1>
+    <h1>Signin As .. </h1>
+    <Link to="/Adminlogin">Admin Login</Link>
+    <Link to="/Userlogin">User Login</Link>
 </div>
     )
 }
